21ba534a2738d0e0c0072ef7c4e8334c7a378ee2
Rubee <rubeevats6@gmail.com>
2025-01-31 00:51:03 +0530

    Update Productfeed.js

--- src/pages/components/Productfeed.js
+++ src/pages/components/Productfeed.js
@@ -3,6 +3,8 @@ import Product from './Product';
 
 
 function Productfeed({ products }) {
+
+    products = products || [];
     return (
         <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
             {products.map(({ id, title, price, description, category, image }) => (
